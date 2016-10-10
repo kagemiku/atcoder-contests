3e31bd94eba18cf501c69a4948507cb82fa41343
KAGE <kagemiku@gmail.com>
2016-10-10 12:25:26 +0900

    Add build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+if [ $# -ne 1 ]; then
+    echo "Invalid argument"
+    echo "Please run './run target_name'"
+    exit 1
+fi
+
+CPP="g++-6"
+CPP_FLAGS="-std=c++1y"
+TARGET=$1
+
+RESULT=`$CPP $CPP_FLAGS -o $TARGET $TARGET.cpp`
+echo $RESULT
+
