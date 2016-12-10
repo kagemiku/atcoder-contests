2faafdc5e6db1a0aebac00f5392880ea45db0a27
KAGE <kagemiku@gmail.com>
2016-12-10 22:33:48 +0900

    Add sources

---
+++ abc/049/c.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define FOR(i,a,b)  for(int (i)=(a);(i)<(b);(i)++)
+#define REP(i,n)    FOR(i,0,n)
+#define RER(i,n)    FOR(i,0,(n+1))
+#define pb          push_back
+#define mp(a,b)     make_pair((a),(b))
+#define mset(m,v)   memset(m,v,sizeof(m))
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef vector<pair<int, int> > vpii;
+typedef long long ll;
+typedef vector<ll> vl;
+typedef pair<ll,ll> pll;
+typedef vector<pair<ll, ll> > vpll;
+typedef vector<string> vs;
+typedef long double ld;
+const double EPS    = 1e-10;
+const int INF       = 1e+9;
+const double PI     = acos(-1);
+
+
+auto main() -> int
+{
+    string s;
+    cin >> s;
+
+    string ss[] = {"eraser", "erase", "dreamer", "dream"};
+    while ( s.length() != 0 ) {
+        bool flag = false;
+        REP(i, 4) {
+            int pos = s.rfind(ss[i]);
+            if ( pos != string::npos && pos == s.length() - ss[i].length() ) {
+                flag = true;
+                s.erase(pos, ss[i].length());
+            }
+        }
+        if ( !flag ) {
+            cout << "NO" << endl;
+            return 0;
+        }
+    }
+
+    cout << "YES" << endl;
+
+    return 0;
+}
+
